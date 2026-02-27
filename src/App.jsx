25c8d88cd859f97807cfda89450a9d65d89a01c2
Hedi Temani <hedi.temani97@gmail.com>
2026-02-27 10:29:06 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { GameHeader } from "./components/GameHeader"
+
+function App() {
+
+  return (
+  <div className="app">
+    <GameHeader />  
+  </div>
+)}
+
+export default App
---
+++ src/components/GameHeader.jsx
@@ -0,0 +1,7 @@
+export const GameHeader = () => {
+    return (
+        <header className="game-header">
+            <h1>Memory Card Game</h1>
+        </header>
+    )
+}
